e314496f179568158cdc7543acafce6d9ed30c9e
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-11-17 10:41:30 +0300

    Dogs Coming

---
+++ app.js
@@ -0,0 +1,47 @@
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const main = document.querySelector(".main");
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "You Love Dogs <3";
+      main.appendChild(img);
+    });
+}
+
+document.querySelector(".images").addEventListener("click", addNewDoggo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
